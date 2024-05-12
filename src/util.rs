ba651cb55cb1fa4182ff0d9c5d0e3288d64f6b4f
Michael Huang <mhuang74@gmail.com>
2024-05-13 07:57:13 +0800

    add trace

--- src/util.rs
+++ src/util.rs
@@ -2,20 +2,21 @@
 
 #[macro_export]
 macro_rules! write_response {
-    ($stream:expr, $response_buffer:expr, $vector_of_bytes:expr) => {{
+    ($stream:expr, $response_buffer:expr, $vector_of_byte_arrays:expr) => {{
         use $crate::resp_protocol::Resp;
-        let response = if $vector_of_bytes.len() > 1 {
-            let bulk_strings: Vec<Resp> = $vector_of_bytes
+        let response = if $vector_of_byte_arrays.len() > 1 {
+            let bulk_strings: Vec<Resp> = $vector_of_byte_arrays
                 .iter()
                 .map(|s| Resp::BulkString(s))
                 .collect();
             Resp::Array(bulk_strings)
         } else {
-            Resp::BulkString($vector_of_bytes.first().unwrap())
+            Resp::BulkString($vector_of_byte_arrays.first().unwrap())
         };
         $response_buffer.clear();
         if response.write_to_writer(&mut $response_buffer).is_ok() {
             $stream.write_all(&$response_buffer).await?;
+            println!("Sent: {}", String::from_utf8_lossy(&$response_buffer));
         }
     }};
 }
